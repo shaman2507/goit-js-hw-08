589445ef921aaa78eac96f9c4a5d230915f4e0e0
shaman2507 <alabay174@gmail.com>
2023-05-23 14:49:17 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
+import simpleLightbox from "simplelightbox";
+import throttle from "lodash.throttle";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+const markup = galleryItems.map(item =>
+    `<li class="gallery_item">
+    <a class="gallery_link" href="${item.original}">
+        <img class="gallery_image" src="${item.preview}" alt="${item.description}" />
+    </a>
+    </li>`
+).join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+const lightbox = new simpleLightbox('.gallery a',
+    { captionsData: "alt", captionDelay: 250 });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,3 @@
-
+import simpleLightbox from "simplelightbox";
+import throttle from "lodash.throttle";
+import "simplelightbox/dist/simple-lightbox.min.css";

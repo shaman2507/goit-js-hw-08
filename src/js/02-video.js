4b09a11aeb9413b0751906898636d901adbc6cff
shaman2507 <alabay174@gmail.com>
2023-05-24 18:26:10 +0200

    update 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,23 @@
-import simpleLightbox from "simplelightbox";
+import Player from "@vimeo/player";
 import throttle from "lodash.throttle";
-import "simplelightbox/dist/simple-lightbox.min.css";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+const timeLocalStorage = localStorage.getItem(LOCAL_STORAGE_KEY) || 0;
+
+const onPlay = function (data) {
+    const currentTime = data.seconds;
+    localStorage.setItem(LOCAL_STORAGE_KEY, currentTime);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(timeLocalStorage).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
